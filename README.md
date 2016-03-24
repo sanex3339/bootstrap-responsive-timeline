1d998c2facf91df42afd69c3722f40c3ab501aee
kachalov <t.kachalov@advmaker.net>
2016-03-24 15:13:54 +0300

    version 1.1.1

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ bower install bootstrap-responsive-timeline --save
 
 Then import `timeline/timeline.less` file from `bower` vendor directory to your `less` file.
 
+###Twitter Bootstrap v3 not included as dependency for this package. 
+###You must include Twitter Bootstrap v3 in your project manually in any possible way.
+
 ###Its important to use `<li>` tags for `timeline-item` and `<ol>` tags for `timeline-label`!
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
+    rename = require('gulp-rename'),
     minifyCss = require('gulp-minify-css');
 
 
-gulp.task('default', ['build-less']);
+gulp.task('default', ['build-less-min', 'build-less']);
+
+gulp.task('build-less-min', function () {
+    gulp.src('less/timeline.less')
+        .pipe(less())
+        .pipe(minifyCss())
+        .pipe(rename('timeline.min.css'))
+        .pipe(gulp.dest('css/'));
+});
 
 gulp.task('build-less', function () {
     gulp.src('less/timeline.less')
         .pipe(less())
-        .pipe(minifyCss())
         .pipe(gulp.dest('css/'));
 });
