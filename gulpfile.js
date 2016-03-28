690082fd2998de250a8f14f36c2944aec7b197cc
kachalov <t.kachalov@advmaker.net>
2016-03-28 18:03:49 +0300

    Scss sources refactoring, removes sass sources

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ var gulp = require('gulp'),
 
 gulp.task('default', ['build-less-min', 'build-less']);
 gulp.task('less', ['build-less-min', 'build-less']);
-gulp.task('sass', ['build-sass-min', 'build-sass']);
 gulp.task('scss', ['build-scss-min', 'build-scss']);
 
 gulp.task('build-less-min', function () {
@@ -22,20 +21,6 @@ gulp.task('build-less-min', function () {
 gulp.task('build-less', function () {
     gulp.src('less/timeline.less')
         .pipe(less())
-        .pipe(gulp.dest('css/'));
-});
-
-gulp.task('build-sass-min', function () {
-    gulp.src('sass/timeline.sass')
-        .pipe(sass())
-        .pipe(minifyCss())
-        .pipe(rename('timeline.min.css'))
-        .pipe(gulp.dest('css/'));
-});
-
-gulp.task('build-sass', function () {
-    gulp.src('sass/timeline.sass')
-        .pipe(sass({outputStyle: 'expanded'}))
         .pipe(gulp.dest('css/'));
 });
 
